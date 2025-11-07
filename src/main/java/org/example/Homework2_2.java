b1b85b2a24ecf7b3c1380dc229bab97343d76547
Nikita <hikitafil97@gmail.com>
2025-11-07 23:57:58 +0300

    add local repository from remote

--- src/main/java/org/example/Homework2_2.java
+++ src/main/java/org/example/Homework2_2.java
@@ -1,5 +1,13 @@
-package org.example;
-
+// Задание 1
 public class Homework2_2 {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
 }
-final 5
+// Задание 2
